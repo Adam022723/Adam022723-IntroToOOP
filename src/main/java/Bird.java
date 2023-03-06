51c8429dfa25fa48f54c717f4e5c19831a014c59
Adam Southard <adam.southard1@outlook.com>
2023-03-06 23:22:08 +0000

    Complete

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -27,7 +27,8 @@
  * 
  * 
  */
-public class Bird {
+public class Bird 
+{
     /**
      * A variable (in this context, often referred to as a field), which represents the age of the bird.
      */
@@ -37,15 +38,18 @@ public class Bird {
      * A constructor for the bird class: this method allows us to build a new bird with the 'new' keyword.
      * No need to change anything here, for now.
      */
-    public Bird(){
-
+    public Bird()
+    {
     }
     /**
      * @return "Tweet" if ageMonths is less than 3 and "Chirp" if ageMonths is
      *         greater than or equal to 3.
      */
-    public String sound(){
-        return "";
+    public String sound()
+    {
+        if(ageMonths < 3)
+        return "Tweet";
+        else return "Chirp";
     }
 
 }
